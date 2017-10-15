f0d052acc7f059678848a5356d50df59c9d723c4
simonvonau <vonau.simon@hotmail.fr>
2017-10-15 15:23:15 +0200

    fixed some bugs + finished enemies colliders

---
+++ restarted_v1/release_2017_10_10_linux/readme.txt
@@ -0,0 +1,61 @@
+
+							Welcome on the piTux project!
+
+
+This project is still in progress and can contains all kinds of bugs.
+
+
+1) Please make sur you already have gcc and g++ installed otherwise type
+>>>sudo apt-get install gcc
+>>>sudo apt-get install g++
+
+2) Install the SDL2
+>>>sudo apt-get install libsdl2-dev
+
+3) Install the SDL2 ttf
+>>>sudo apt-get install libsdl2-ttf-dev
+
+4) Install the SDL2 image
+>>>sudo apt-get install libsdl2-image-dev
+
+5) Install the SDL2 mixer
+>>>sudo apt-get install libsdl2-mixer-dev
+
+6) Install the git client
+>>>sudo apt-get install git
+
+7) Download the game
+>>>git clone https://github.com/simonvonau/piTux
+
+8) You can now compile he project by running the makefile
+>>>sh makefile
+
+9) Finally to run the game
+>>>./piTux_exe
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ restarted_v1/release_2017_10_10_linux/src/model/heroInstance.h
@@ -0,0 +1,69 @@
+#ifndef __HEROINSTANCE_H__
+#define __HEROINSTANCE_H__
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> // memcpy strncpy
+
+#include "../model/collider.h"
+#include "../model/heros.h"
+#include "../utils/reporting.h"
+#include "../global.h"
+#include "../utils/arrayProcessing.h"
+
+
+typedef struct{
+    int idHeros;
+    Collider *** herosColl;//[currState][currActionid]
+    int currState;// Small tux, big Tux, fire Tux
+    int currAction;// Jumping, walking, ...
+    int currSprite;// The current sprite of the action
+    int isDead;
+    int lifesLeft;
+    int nbCoins;
+    int lastMovementTime;// Last time when tux move
+    int isTouchingGround;// If the heros is on the ground (or on an ennemy)
+    int jumpKeyPressed;// (0 or 1)When player press the jump key
+    int fireKeyPressed;// (0 or 1)
+    int leftKeyPressed;// (0 or 1)
+    int rightKeyPressed;// (0 or 1)
+    int jumpStartTime;// Time at jump start
+    int jumpDuration;// Time after jump starts
+    char lastDirection;// 'r' or 'l'
+    int posX, posY, lastPosX, lastPosY;
+    float movementProgressX, movementProgressY; // Store a movement <1 pixel => to avoid being stuck if FPS is too fast
+    int currentTime;
+    int godModeDuration; // The god mode allow tux to touch enemies without troubles
+    int timeBeforeNextShot;
+    int hasReleaseJumpKey;
+    int hasReleaseFireKey;
+    int currentLevelNo;
+}HeroInstance;
+
+HeroInstance * initHeroInstance();
+
+void resetHerosInstanceBetweenLevel(HeroInstance *p_herosInstance);
+
+void changeHerosState(HeroInstance * p_herosInstance,int p_newState, int p_newAction, int p_newSprite);
+void changeHerosAction(HeroInstance * p_herosInstance, int p_newAction, int p_newSprite);
+
+// Refreshing the heros
+void refreshHerosInstance(HeroInstance * p_herosInstance, Heros *p_heros, int p_currentTime, int p_loopTime);
+void applyGravity(HeroInstance *p_herosInstance, int p_loopTime);
+void movingRight(HeroInstance *p_herosInstance, Heros *p_heros, int p_loopTime);
+void movingLeft(HeroInstance *p_herosInstance, Heros *p_heros, int p_loopTime);
+void jump(HeroInstance *p_herosInstance, Heros *p_heros, int p_loopTime);
+void land(HeroInstance *p_herosInstance);
+void updateCurrentSprite(HeroInstance *p_heroInstance, Heros *p_heros);
+
+void updTuxSpriteLocFromCollLoc(HeroInstance *p_heroInstance, Heros *p_heros);
+
+void gatherCoin(HeroInstance *p_heroInstance);
+void heroInstanceDeath(HeroInstance *p_herosInstance, Heros *p_heros, int p_currentTime);
+
+void destroyHeroInstanceColliders(HeroInstance *p_heroInstance, Heros *p_heros);
+void destroyHeroInstance(HeroInstance *p_heroInstance);
+
+
+
+
+#endif
